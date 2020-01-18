5d7939090770cd293c94e0fdbff8dc5420e16527
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-01-18 21:17:52 +0000

    v2

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,11 +1,15 @@
 let img;
-function preload() {
+
+function preload() { // runs once
+  img = loadImage ('images/planet earth.jpg');
 }
 
 function setup(){ // happens once
 
   createCanvas (windowWidth,windowHeight);
   background(50); // 1
+  imageMode(CENTER);
+  image(img, 10, 10, 50, 50);
 }
 
 function mousePressed () { // Event
