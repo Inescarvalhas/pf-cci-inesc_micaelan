de4bd8e1ce9f6521f89c66a0ef2251e784fac318
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-01-21 21:32:41 +0000

    v4
    load image

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,13 +1,16 @@
-let img;
+var img
 
 function preload() { // runs once
-  img = loadImage ( 'C:\Users\Asus\Documents\GitHub\pf-cci-inesc_micaelan\images/planet earth.jpg')
+  img = loadImage ('https://github.com/ESELX/pf-cci-inesc_micaelan/blob/master/images/planet%20earth.jpg');
 }
 
 function setup(){ // happens once
-
   createCanvas (windowWidth,windowHeight);
   background(50); // 1
+
+  img = createImg  ('https://github.com/ESELX/pf-cci-inesc_micaelan/blob/master/images/planet%20earth.jpg')
+  // (100, 100);
+  img.loadPixels();
   imageMode(CENTER);
   image(img, 10, 10, 50, 50);
 }
