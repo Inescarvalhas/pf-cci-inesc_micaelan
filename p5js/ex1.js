2c2a71d904c97f55bfdce0c1d9ae98a2b1cfe5cd
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-01-23 15:07:01 +0000

    v7
    falta load image

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -3,25 +3,31 @@ var img
 var w = window.innerWidth;
 var h = window.innerHeight;
 
+
 function preload() { // runs once
 img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
 }
 
 function setup(){ // happens once
-  createCanvas (window.innerWidth, window.innerHeight);
-  background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
+  createCanvas (1500, 690);//(window.innerWidth,window.innerHeight);
 
-  //img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/planet%20earth.jpg')
+//  img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') class="over";
+  //imageMode (CENTER);
+  //image (img , 50,50);
+
   //img.loadPixels();
-  //imageMode (CENTER)
   //image(img, 0, 0);
+
+
 }
 
 function mousePressed () { // Event
 }
 
 function draw() { // happens forever.. loop
-
+  background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
+  fill (50,50,255); // Red Green Blue 0 preto 255 branco
+  circle(750, 345, 400); // (x, y, d)
 
   //textSize(30);
  //text ( 'Espectro Eletromagnético',70,30);
