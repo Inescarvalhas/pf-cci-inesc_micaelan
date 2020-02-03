4ca59ee8861d20de32b46aad5474a98bec9ee692
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-02-03 17:19:03 +0000

    v9
    image loop

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,7 +1,5 @@
 let image
 var img
-var w = window.innerWidth;
-var h = window.innerHeight;
 
 
 function preload() { // runs once
@@ -9,15 +7,13 @@ img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/
 }
 
 function setup(){ // happens once
-  createCanvas (1500, 690);//(window.innerWidth,window.innerHeight);
-
-//  img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') class="over";
+  createCanvas (1500, 690);
+  img =  createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') ;
   //imageMode (CENTER);
   //image (img , 50,50);
 
   //img.loadPixels();
   //image(img, 0, 0);
-
 
 }
 
@@ -26,6 +22,7 @@ function mousePressed () { // Event
 
 function draw() { // happens forever.. loop
   background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
+  img =  createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') ;
   fill (50,50,255); // Red Green Blue 0 preto 255 branco
   circle(750, 345, 400); // (x, y, d)
   //nope img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') class="over";
