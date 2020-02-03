b3cd8ce99f4379d322fab2c3ab388bef11418681
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-02-03 18:13:56 +0000

    v10
    trying

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,4 +1,4 @@
-let image
+let image  // Declare variable 'img'
 var img
 
 
@@ -8,10 +8,9 @@ img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/
 
 function setup(){ // happens once
   createCanvas (1500, 690);
+  img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
   img =  createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') ;
   //imageMode (CENTER);
-  //image (img , 50,50);
-
   //img.loadPixels();
   //image(img, 0, 0);
 
@@ -22,9 +21,10 @@ function mousePressed () { // Event
 
 function draw() { // happens forever.. loop
   background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
-  img =  createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') ;
-  fill (50,50,255); // Red Green Blue 0 preto 255 branco
-  circle(750, 345, 400); // (x, y, d)
+  image(img, 0, 0);
+  //
+//  fill (50,50,255); // Red Green Blue 0 preto 255 branco
+  //circle(750, 345, 400); // (x, y, d)
   //nope img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') class="over";
 
   //textSize(30);
