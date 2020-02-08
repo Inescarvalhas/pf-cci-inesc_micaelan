5feb71bc695c988d4cffc0cc78285f983686b147
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-02-08 11:07:42 +0000

    v12
    buttons !!!! in progress

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,41 +1,40 @@
 let earth // Declare variable 'img'
 let sky
-var img
+let diamEarth
+let button
 
 
 function preload() { // runs once
 
-sky = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/universe-sky.jpg')
-earth = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
+sky = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/sky.jpg', 'sky')
+earth = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png','earth');
 }
 
-function setup(){ // happens once
+function setup() { // happens once
   createCanvas (window.innerWidth, window.innerHeight);
- // sky = createImg ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/universe-sky.jpg', 'sky')
 
   background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
   image (sky, 0 , 0, window.innerWidth, window.innerHeight);
-  image (earth,100,100); // (img, x, y, w, h)
-  blend (earth, 0, 0, 33, 100, 67, 0, 33, 100, LIGHTEST);
+  imageMode (CENTER);
+  image (earth,width/2, height/2); // (img, x, y, w, h)
 
-  //imageMode (CENTER);
-  //img.loadPixels();
+  diamEarth = 200
 
-
+  button = createButton('Radio');
+  button.position(19, 19);
+  button.mousePressed(changeRadio);
 }
+
 
 function mousePressed () { // Event
 }
 
+function changeRadio() {
+  let radio = 
+
 function draw() { // happens forever.. loop
+}
 
-
-
-
-//fill (50,50,255); // Red Green Blue 0 preto 255 branco
-//circle(750, 345, 400); // (x, y, d)
-//textSize(30);
-//text ( 'Espectro Eletromagnético',70,30);
-//fill(255);
-
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
