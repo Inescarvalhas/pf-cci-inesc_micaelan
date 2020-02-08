8049230b07467548b35295244232163e3bcb01ed
ellaneves <47819485+ellaneves@users.noreply.github.com>
2020-02-08 09:45:33 +0000

    v11
    Fundo ok

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,18 +1,26 @@
-let image  // Declare variable 'img'
+let earth // Declare variable 'img'
+let sky
 var img
 
 
 function preload() { // runs once
-img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
+
+sky = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/universe-sky.jpg')
+earth = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
 }
 
 function setup(){ // happens once
-  createCanvas (1500, 690);
-  img = loadImage ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png');
-  img =  createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') ;
+  createCanvas (window.innerWidth, window.innerHeight);
+ // sky = createImg ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/universe-sky.jpg', 'sky')
+
+  background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
+  image (sky, 0 , 0, window.innerWidth, window.innerHeight);
+  image (earth,100,100); // (img, x, y, w, h)
+  blend (earth, 0, 0, 33, 100, 67, 0, 33, 100, LIGHTEST);
+
   //imageMode (CENTER);
   //img.loadPixels();
-  //image(img, 0, 0);
+
 
 }
 
@@ -20,14 +28,14 @@ function mousePressed () { // Event
 }
 
 function draw() { // happens forever.. loop
-  background (0); // 0 preto 255 branco; // Fundo,Fundo,Fundo
-  image(img, 0, 0);
-  //
-//  fill (50,50,255); // Red Green Blue 0 preto 255 branco
-  //circle(750, 345, 400); // (x, y, d)
-  //nope img = createImg  ('https://raw.githubusercontent.com/ESELX/pf-cci-inesc_micaelan/master/images/Earth.png') class="over";
 
-  //textSize(30);
- //text ( 'Espectro Eletromagnético',70,30);
-  //fill(255);
+
+
+
+//fill (50,50,255); // Red Green Blue 0 preto 255 branco
+//circle(750, 345, 400); // (x, y, d)
+//textSize(30);
+//text ( 'Espectro Eletromagnético',70,30);
+//fill(255);
+
 }
